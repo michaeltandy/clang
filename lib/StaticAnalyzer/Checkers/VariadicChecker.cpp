db5c117d3cdc5287829dd4e55166aeb2ef1ff08a
mtandy <mtandy@mtandy-desktop>
2016-07-26 23:57:55 +0100

    Checks for out-of-sequence calls to stdarg functions

--- lib/StaticAnalyzer/Checkers/VariadicChecker.cpp
+++ lib/StaticAnalyzer/Checkers/VariadicChecker.cpp
@@ -46,7 +46,8 @@ public:
 
 
 class VariadicChecker : public Checker<check::PostCall,
-										check::PreCall> {
+										check::PreCall,
+										check::PreStmt<Stmt>> {
   CallDescription VaStart;
   CallDescription VaEnd;
 
@@ -55,6 +56,19 @@ public:
 
   void checkPostCall(const CallEvent &Call, CheckerContext &C) const;
   void checkPreCall(const CallEvent &Call, CheckerContext &C) const;
+  void checkPreStmt(const Stmt *CE, CheckerContext &C) const;
+
+private:
+  mutable std::unique_ptr<BuiltinBug> doubleStartBug;
+  void initDoubleStartBug() const;
+  mutable std::unique_ptr<BuiltinBug> doubleEndBug;
+  void initDoubleEndBug() const;
+  mutable std::unique_ptr<BuiltinBug> endWithoutStartBug;
+  void initEndWithoutStartBug() const;
+  mutable std::unique_ptr<BuiltinBug> getWhenNotStartedBug;
+  void initGetWhenNotStartedBug() const;
+
+
 };
 
 } // End anonymous namespace
@@ -69,20 +83,20 @@ VariadicChecker::VariadicChecker()
 void VariadicChecker::checkPostCall(const CallEvent &Call, CheckerContext &C) const {
 	if (Call.isCalled(VaStart)) {
 		ProgramStateRef state = C.getState();
-		llvm::outs() << "\n\nVaStart checkPostCall:\n";
+		//llvm::outs() << "\n\nVaStart checkPostCall:\n";
 		//state->dump();
 
 		const ImplicitCastExpr *implicitCastVaList = cast<ImplicitCastExpr>(Call.getArgExpr(0));
-		llvm::outs() << "implicitCastVaList:\n";
-		implicitCastVaList->dump(llvm::outs());
+		//llvm::outs() << "implicitCastVaList:\n";
+		//implicitCastVaList->dump(llvm::outs());
 
 		SVal vaListSVal = state->getSVal(implicitCastVaList, C.getLocationContext());
-		llvm::outs() << "vaListSVal:\n";
-		vaListSVal.dumpToStream(llvm::outs());
+		//llvm::outs() << "vaListSVal:\n";
+		//vaListSVal.dumpToStream(llvm::outs());
 
 		const MemRegion *vaListRegion = vaListSVal.getAsRegion();
-		llvm::outs() << "\nvaListRegion: " << (const void*)vaListRegion << "\n";
-		vaListRegion->dumpToStream(llvm::outs());
+		//llvm::outs() << "\nvaListRegion: " << (const void*)vaListRegion << "\n";
+		//vaListRegion->dumpToStream(llvm::outs());
 
 		/*if (vaListSVal.getAsSymbol()) llvm::outs() << "getAsSymbol!\n";
 		if (vaListSVal.getAsLocSymbol()) llvm::outs() << "getAsLocSymbol!\n";
@@ -92,6 +106,15 @@ void VariadicChecker::checkPostCall(const CallEvent &Call, CheckerContext &C) co
 		if (vaListSVal.getAsSymbolicExpression()) llvm::outs() << "getAsSymbolicExpression!\n";
 		if (vaListSVal.getAsFunctionDecl()) llvm::outs() << "getAsFunctionDecl!\n";*/
 
+		const VaListState *vaListState = state->get<VaListMap>(vaListRegion);
+		if (vaListState!=0 && vaListState->isStarted()) {
+			initDoubleStartBug();
+			ExplodedNode *N = C.generateNonFatalErrorNode();
+			auto R = llvm::make_unique<BugReport>(*doubleStartBug, doubleStartBug->getDescription(), N);
+				  R->addRange(Call.getSourceRange());
+				  C.emitReport(std::move(R));
+		}
+
 		state = state->set<VaListMap>(vaListRegion, VaListState::getStarted());
 		C.addTransition(state);
 	}
@@ -100,34 +123,109 @@ void VariadicChecker::checkPostCall(const CallEvent &Call, CheckerContext &C) co
 void VariadicChecker::checkPreCall(const CallEvent &Call, CheckerContext &C) const {
 	if (Call.isCalled(VaEnd)) {
 		ProgramStateRef state = C.getState();
-		llvm::outs() << "\n\n\n\nVaEnd checkPreCall:\n";
+		//llvm::outs() << "\n\n\n\nVaEnd checkPreCall:\n";
 		//state->dump();
 
 		const ImplicitCastExpr *implicitCastVaList = cast<ImplicitCastExpr>(Call.getArgExpr(0));
-		llvm::outs() << "implicitCastVaList:\n";
-		implicitCastVaList->dump(llvm::outs());
+		//llvm::outs() << "implicitCastVaList:\n";
+		//implicitCastVaList->dump(llvm::outs());
 
 		SVal vaListSVal = state->getSVal(implicitCastVaList, C.getLocationContext());
-		llvm::outs() << "vaListSVal:\n";
-		vaListSVal.dumpToStream(llvm::outs());
+		//llvm::outs() << "vaListSVal:\n";
+		//vaListSVal.dumpToStream(llvm::outs());
 
 		const MemRegion *vaListRegion = vaListSVal.getAsRegion();
-		llvm::outs() << "\nvaListRegion: " << (const void*)vaListRegion << "\n";
-		vaListRegion->dumpToStream(llvm::outs());
+		//llvm::outs() << "\nvaListRegion: " << (const void*)vaListRegion << "\n";
+		//vaListRegion->dumpToStream(llvm::outs());
 
 		const VaListState *vaListState = state->get<VaListMap>(vaListRegion);
 		if (vaListState == 0) {
-			llvm::outs() << "\n vaListState: Null\n";
-		} else if (vaListState->isStarted()) {
-			llvm::outs() << "\n vaListState: Started\n";
+			initEndWithoutStartBug();
+			ExplodedNode *N = C.generateNonFatalErrorNode();
+			auto R = llvm::make_unique<BugReport>(*endWithoutStartBug, endWithoutStartBug->getDescription(), N);
+				  R->addRange(Call.getSourceRange());
+				  C.emitReport(std::move(R));
 		} else if (vaListState->isEnded()) {
-			llvm::outs() << "\n vaListState: Ended\n";
-		} else {
-			llvm::outs() << "\n vaListState: Confusing\n";
+			initDoubleEndBug();
+			ExplodedNode *N = C.generateNonFatalErrorNode();
+			auto R = llvm::make_unique<BugReport>(*doubleEndBug, doubleEndBug->getDescription(), N);
+				  R->addRange(Call.getSourceRange());
+				  C.emitReport(std::move(R));
 		}
 
 		state = state->set<VaListMap>(vaListRegion, VaListState::getEnded());
 		C.addTransition(state);
+	}
+}
+
+void VariadicChecker::checkPreStmt(const Stmt *S, CheckerContext &C) const {
+  if (S->getStmtClass() != Stmt::VAArgExprClass)
+    return;
+
+  ProgramStateRef state = C.getState();
+  const VAArgExpr *VAE = cast<VAArgExpr>(S);
+
+  llvm::outs() << "\n\nVAArgExpr:\n";
+  VAE->dump(llvm::outs());
+
+  // Revisit - are there alternatives to these options?
+  assert(!VAE->isMicrosoftABI());
+  assert(C.getASTContext().getBuiltinVaListType()->isArrayType());
+  assert(isa<ImplicitCastExpr>(VAE->getSubExpr()));
+  const ImplicitCastExpr *implicitCastVaList = cast<ImplicitCastExpr>(VAE->getSubExpr());
+
+  SVal vaListSVal = state->getSVal(implicitCastVaList, C.getLocationContext());
+  const MemRegion *vaListRegion = vaListSVal.getAsRegion();
+  llvm::outs() << "\nvaListRegion: " << (const void*)vaListRegion << "\n";
+  vaListRegion->dumpToStream(llvm::outs());
+
+  const VaListState *vaListState = state->get<VaListMap>(vaListRegion);
+  if (vaListState == 0 || vaListState->isEnded()) {
+	  initGetWhenNotStartedBug();
+	  ExplodedNode *N = C.generateNonFatalErrorNode();
+	  auto R = llvm::make_unique<BugReport>(*getWhenNotStartedBug, getWhenNotStartedBug->getDescription(), N);
+	  	  R->addRange(S->getSourceRange());
+	  	  C.emitReport(std::move(R));
+	  return;
+  }
+  assert(vaListState->isStarted());
+
+}
+
+void VariadicChecker::initDoubleStartBug() const {
+	if (!doubleStartBug) {
+		doubleStartBug.reset(
+	        new BuiltinBug(this, "va_start when already started",
+	               "va_start should not be invoked twice "
+	               "unless there's a call to va_end in between."));
+	}
+}
+
+void VariadicChecker::initDoubleEndBug() const {
+	if (!doubleEndBug) {
+		doubleEndBug.reset(
+	        new BuiltinBug(this, "va_end called more than once",
+	               "va_end should only be invoked once for each "
+	               "call to va_start or va_copy."));
+	}
+}
+
+void VariadicChecker::initEndWithoutStartBug() const {
+	if (!endWithoutStartBug) {
+		endWithoutStartBug.reset(
+	        new BuiltinBug(this, "va_end called without va_start",
+	               "va_end should only be invoked for variables "
+	               "first populated by va_start or va_copy."));
+	}
+}
+
+void VariadicChecker::initGetWhenNotStartedBug() const {
+	if (!getWhenNotStartedBug) {
+		getWhenNotStartedBug.reset(
+	        new BuiltinBug(this, "va_arg called before va_start or after va_end",
+	               "va_arg should only be invoked with a parameter "
+	               "initialised by va_start or va_copy, "
+	        	   "and not yet cleaned up by va_end."));
 	}
 }
 
